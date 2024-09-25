e435eadb41d873a831ab1fae318226927ace98ae
Jaz <ericvolp12@gmail.com>
2024-09-24 22:24:15 -0700

    Allow options updates after stream initializes

---
+++ pkg/server/server.go
@@ -0,0 +1,306 @@
+package server
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/bluesky-social/jetstream/pkg/consumer"
+	"github.com/bluesky-social/jetstream/pkg/models"
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+	"go.opentelemetry.io/otel"
+	"golang.org/x/sync/semaphore"
+)
+
+type Server struct {
+	Subscribers map[int64]*Subscriber
+	lk          sync.RWMutex
+	nextSub     int64
+	Consumer    *consumer.Consumer
+	maxSubRate  float64
+	seq         int64
+}
+
+var upgrader = websocket.Upgrader{}
+var maxConcurrentEmits = int64(100)
+var cutoverThresholdUS = int64(1_000_000)
+var tracer = otel.Tracer("jetstream-server")
+
+func NewServer(maxSubRate float64) (*Server, error) {
+	s := Server{
+		Subscribers: make(map[int64]*Subscriber),
+		maxSubRate:  maxSubRate,
+	}
+
+	return &s, nil
+}
+
+func (s *Server) GetSeq() int64 {
+	s.lk.RLock()
+	defer s.lk.RUnlock()
+	return s.seq
+}
+
+func (s *Server) SetSeq(seq int64) {
+	s.lk.Lock()
+	defer s.lk.Unlock()
+	s.seq = seq
+}
+
+func (s *Server) HandleSubscribe(c echo.Context) error {
+	ctx, cancel := context.WithCancel(c.Request().Context())
+	defer cancel()
+
+	qWantedCollections := c.Request().URL.Query()["wantedCollections"]
+	qWantedDids := c.Request().URL.Query()["wantedDids"]
+
+	// Check if the user wants to initialize options over the websocket before subscribing
+	requireHello := c.Request().URL.Query().Get("requireHello") == "true"
+
+	// Check if the user wants zstd compression, can be set in the header or query param
+	socketEncoding := c.Request().Header.Get("Socket-Encoding")
+	compress := strings.Contains(socketEncoding, "zstd") || c.Request().URL.Query().Get("compress") == "true"
+
+	// Parse the cursor
+	var cursor *int64
+	var err error
+	qCursor := c.Request().URL.Query().Get("cursor")
+	if qCursor != "" {
+		cursor = new(int64)
+		*cursor, err = strconv.ParseInt(qCursor, 10, 64)
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("invalid cursor: %s", qCursor))
+			return fmt.Errorf("invalid cursor: %s", qCursor)
+		}
+
+		// If given a future cursor, just live tail
+		if *cursor > time.Now().UnixMicro() {
+			cursor = nil
+		}
+	}
+
+	// Parse the subscriber options
+	subscriberOpts, err := parseSubscriberOptions(ctx, qWantedCollections, qWantedDids, compress, cursor)
+	if err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return err
+	}
+
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return err
+	}
+	defer ws.Close()
+
+	log := slog.With("source", "server_handle_subscribe", "socket_addr", ws.RemoteAddr().String(), "real_ip", c.RealIP())
+
+	sub, err := s.AddSubscriber(ws, c.RealIP(), subscriberOpts)
+	if err != nil {
+		log.Error("failed to add subscriber", "error", err)
+		return err
+	}
+	defer s.RemoveSubscriber(sub.id)
+
+	// Handle incoming messages
+	go func() {
+		for {
+			msgType, msgBytes, err := ws.ReadMessage()
+			if err != nil {
+				log.Error("failed to read message from websocket", "error", err)
+				cancel()
+				return
+			}
+
+			if len(msgBytes) > 10_000_000 {
+				log.Error("message from subscriber too large, ignoring", "size", len(msgBytes))
+				continue
+			}
+
+			switch msgType {
+			case websocket.PingMessage:
+				if err := ws.WriteMessage(websocket.PongMessage, nil); err != nil {
+					log.Error("failed to write pong to websocket", "error", err)
+					cancel()
+					return
+				}
+			case websocket.CloseMessage:
+				log.Info("received close message from client")
+				cancel()
+				return
+			case websocket.TextMessage:
+				log.Info("received text message from client, parsing as subscriber options update")
+				log.Debug("text message from client content", "msg", string(msgBytes))
+
+				var subOptsUpdate SubscriberOptionsUpdateMsg
+				if err := json.Unmarshal(msgBytes, &subOptsUpdate); err != nil {
+					log.Error("failed to unmarshal subscriber options update", "error", err, "msg", string(msgBytes))
+					sub.Terminate(fmt.Sprintf("failed to unmarshal subscriber options update: %v", err))
+					cancel()
+					return
+				}
+
+				// Only WantedCollections and WantedDIDs can be updated after the initial connection
+				// Cursor and Compression settings are fixed for the lifetime of the stream
+				subscriberOpts, err = parseSubscriberOptions(ctx, subOptsUpdate.WantedCollections, subOptsUpdate.WantedDIDs, compress, sub.cursor)
+				if err != nil {
+					log.Error("failed to parse subscriber options", "error", err, "new_opts", subOptsUpdate)
+					sub.Terminate(fmt.Sprintf("failed to parse subscriber options: %v", err))
+					cancel()
+					return
+				}
+
+				sub.UpdateOptions(subscriberOpts)
+
+				if requireHello {
+					sub.hello <- struct{}{}
+					requireHello = false
+				}
+			case websocket.BinaryMessage:
+				log.Warn("received unexpected binary message from client, ignoring")
+			}
+		}
+	}()
+
+	// If requireHello is set, wait for the client to send a valid options update message before proceeding
+	if requireHello {
+		log.Info("waiting for hello message from client")
+		select {
+		case <-ctx.Done():
+			log.Info("shutting down subscriber")
+			return nil
+		case <-sub.hello:
+			log.Info("received hello message from client, proceeding")
+		}
+	}
+
+	// Replay events if a cursor is provided
+	if sub.cursor != nil {
+		log.Info("replaying events", "cursor", *sub.cursor)
+		playbackRateLimit := s.maxSubRate * 10
+
+		go func() {
+			for {
+				lastSeq, err := s.Consumer.ReplayEvents(ctx, sub.compress, *sub.cursor, playbackRateLimit, func(ctx context.Context, timeUS int64, did, collection string, getEventBytes func() []byte) error {
+					return emitToSubscriber(ctx, log, sub, timeUS, did, collection, true, getEventBytes)
+				})
+				if err != nil {
+					log.Error("failed to replay events", "error", err)
+					sub.Terminate("failed to replay events")
+					cancel()
+					return
+				}
+				serverLastSeq := s.GetSeq()
+				log.Info("finished replaying events", "replay_last_time", time.UnixMicro(lastSeq), "server_last_time", time.UnixMicro(serverLastSeq))
+
+				// If last event replayed is close enough to the last live event, start live tailing
+				if lastSeq > serverLastSeq-(cutoverThresholdUS/2) {
+					break
+				}
+
+				// Otherwise, update the cursor and replay again
+				lastSeq++
+				sub.SetCursor(&lastSeq)
+			}
+			log.Info("finished replaying events, starting live tail")
+			sub.SetCursor(nil)
+		}()
+	}
+
+	// Read events from the outbox and send them to the subscriber
+	for {
+		select {
+		case <-ctx.Done():
+			log.Info("shutting down subscriber")
+			return nil
+		case msg := <-sub.outbox:
+			err := sub.rl.Wait(ctx)
+			if err != nil {
+				log.Error("failed to wait for rate limiter", "error", err)
+				return fmt.Errorf("failed to wait for rate limiter: %w", err)
+			}
+
+			// When compression is enabled, the msg is a zstd compressed message
+			if compress {
+				if err := ws.WriteMessage(websocket.BinaryMessage, *msg); err != nil {
+					log.Error("failed to write message to websocket", "error", err)
+					return nil
+				}
+				continue
+			}
+
+			// Otherwise, the msg is serialized JSON
+			if err := ws.WriteMessage(websocket.TextMessage, *msg); err != nil {
+				log.Error("failed to write message to websocket", "error", err)
+				return nil
+			}
+		}
+	}
+}
+
+func (s *Server) Emit(ctx context.Context, e *models.Event, asJSON, compBytes []byte) error {
+	ctx, span := tracer.Start(ctx, "Emit")
+	defer span.End()
+
+	log := slog.With("source", "server_emit")
+
+	s.lk.RLock()
+	defer func() {
+		s.lk.RUnlock()
+		s.SetSeq(e.TimeUS)
+	}()
+
+	eventsEmitted.Inc()
+	evtSize := float64(len(asJSON))
+	bytesEmitted.Add(evtSize)
+
+	collection := ""
+	if e.EventType == models.EventCommit && e.Commit != nil {
+		collection = e.Commit.Collection
+	}
+
+	// Wrap the valuer functions for more lightweight event filtering
+	getJSONEvent := func() []byte { return asJSON }
+	getCompressedEvent := func() []byte { return compBytes }
+
+	// Concurrently emit to all subscribers
+	// We can't move on until all subscribers have received the event or been dropped for being too slow
+	sem := semaphore.NewWeighted(maxConcurrentEmits)
+	for _, sub := range s.Subscribers {
+		if err := sem.Acquire(ctx, 1); err != nil {
+			log.Error("failed to acquire semaphore", "error", err)
+			return fmt.Errorf("failed to acquire semaphore: %w", err)
+		}
+		go func(sub *Subscriber) {
+			defer sem.Release(1)
+			sub.lk.Lock()
+			defer sub.lk.Unlock()
+
+			// Don't emit events to subscribers that are replaying and are too far behind
+			if sub.cursor != nil && sub.seq < e.TimeUS-cutoverThresholdUS {
+				return
+			}
+
+			// Pick the event valuer for the subscriber based on their compression preference
+			getEventBytes := getJSONEvent
+			if sub.compress {
+				getEventBytes = getCompressedEvent
+			}
+
+			emitToSubscriber(ctx, log, sub, e.TimeUS, e.Did, collection, false, getEventBytes)
+		}(sub)
+	}
+
+	if err := sem.Acquire(ctx, maxConcurrentEmits); err != nil {
+		log.Error("failed to acquire semaphore", "error", err)
+		return fmt.Errorf("failed to acquire semaphore: %w", err)
+	}
+
+	return nil
+}
---
+++ pkg/server/subscriber.go
@@ -0,0 +1,269 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"strings"
+	"sync"
+
+	"github.com/bluesky-social/indigo/atproto/syntax"
+	"github.com/gorilla/websocket"
+	"github.com/prometheus/client_golang/prometheus"
+	"golang.org/x/time/rate"
+)
+
+type WantedCollections struct {
+	Prefixes  []string
+	FullPaths map[string]struct{}
+}
+
+type Subscriber struct {
+	ws     *websocket.Conn
+	realIP string
+	lk     sync.Mutex
+	seq    int64
+	outbox chan *[]byte
+	hello  chan struct{}
+	id     int64
+
+	// Subscriber options
+
+	// wantedCollections is nil if the subscriber wants all collections
+	wantedCollections *WantedCollections
+	wantedDids        map[string]struct{}
+	cursor            *int64
+	compress          bool
+
+	rl *rate.Limiter
+
+	deliveredCounter prometheus.Counter
+	bytesCounter     prometheus.Counter
+}
+
+// emitToSubscriber sends an event to a subscriber if the subscriber wants the event
+// It takes a valuer function to get the event bytes so that the caller can avoid
+// unnecessary allocations and/or reading from the playback DB if the subscriber doesn't want the event
+func emitToSubscriber(ctx context.Context, log *slog.Logger, sub *Subscriber, timeUS int64, did, collection string, playback bool, getEventBytes func() []byte) error {
+	if !sub.WantsCollection(collection) {
+		return nil
+	}
+
+	if len(sub.wantedDids) > 0 {
+		if _, ok := sub.wantedDids[did]; !ok {
+			return nil
+		}
+	}
+
+	// Skip events that are older than the subscriber's last seen event
+	if timeUS <= sub.seq {
+		return nil
+	}
+
+	evtBytes := getEventBytes()
+	if playback {
+		// Copy the event bytes so the playback iterator can reuse the buffer
+		evtBytes = append([]byte{}, evtBytes...)
+		select {
+		case <-ctx.Done():
+			log.Error("failed to send event to subscriber", "error", ctx.Err(), "subscriber", sub.id)
+			// If we failed to send to a subscriber, close the connection
+			sub.Terminate("error sending event")
+			err := sub.ws.Close()
+			if err != nil {
+				log.Error("failed to close subscriber connection", "error", err)
+			}
+			return ctx.Err()
+		case sub.outbox <- &evtBytes:
+			sub.seq = timeUS
+			sub.deliveredCounter.Inc()
+			sub.bytesCounter.Add(float64(len(evtBytes)))
+		}
+	} else {
+		select {
+		case <-ctx.Done():
+			log.Error("failed to send event to subscriber", "error", ctx.Err(), "subscriber", sub.id)
+			// If we failed to send to a subscriber, close the connection
+			sub.Terminate("error sending event")
+			err := sub.ws.Close()
+			if err != nil {
+				log.Error("failed to close subscriber connection", "error", err)
+			}
+			return ctx.Err()
+		case sub.outbox <- &evtBytes:
+			sub.seq = timeUS
+			sub.deliveredCounter.Inc()
+			sub.bytesCounter.Add(float64(len(evtBytes)))
+		default:
+			// Drop slow subscribers if they're live tailing and fall too far behind
+			log.Error("failed to send event to subscriber, dropping", "error", "buffer full", "subscriber", sub.id)
+			sub.Terminate("consuming too slowly")
+			err := sub.ws.Close()
+			if err != nil {
+				log.Error("failed to close subscriber connection", "error", err)
+			}
+		}
+	}
+
+	return nil
+}
+
+type SubscriberOptionsUpdateMsg struct {
+	WantedCollections []string `json:"wantedCollections"`
+	WantedDIDs        []string `json:"wantedDids"`
+}
+
+type SubscriberOptions struct {
+	WantedCollections *WantedCollections
+	WantedDIDs        map[string]struct{}
+	Compress          bool
+	Cursor            *int64
+}
+
+// ErrInvalidOptions is returned when the subscriber options are invalid
+var ErrInvalidOptions = fmt.Errorf("invalid subscriber options")
+
+func parseSubscriberOptions(ctx context.Context, wantedCollectionsProvided, wantedDidsProvided []string, compress bool, cursor *int64) (*SubscriberOptions, error) {
+	ctx, span := tracer.Start(ctx, "parseSubscriberOptions")
+	defer span.End()
+
+	var wantedCol *WantedCollections
+	if len(wantedCollectionsProvided) > 0 {
+		wantedCol = &WantedCollections{
+			Prefixes:  []string{},
+			FullPaths: make(map[string]struct{}),
+		}
+
+		for _, providedCol := range wantedCollectionsProvided {
+			if strings.HasSuffix(providedCol, ".*") {
+				wantedCol.Prefixes = append(wantedCol.Prefixes, strings.TrimSuffix(providedCol, "*"))
+				continue
+			}
+
+			col, err := syntax.ParseNSID(providedCol)
+			if err != nil {
+
+				return nil, fmt.Errorf("%w: invalid collection: %s", ErrInvalidOptions, providedCol)
+			}
+			wantedCol.FullPaths[col.String()] = struct{}{}
+		}
+	}
+
+	didMap := make(map[string]struct{})
+	for _, d := range wantedDidsProvided {
+		did, err := syntax.ParseDID(d)
+		if err != nil {
+			return nil, ErrInvalidOptions
+		}
+		didMap[did.String()] = struct{}{}
+	}
+
+	// Reject requests with too many wanted DIDs
+	if len(didMap) > 10_000 {
+		return nil, fmt.Errorf("%w: too many wanted DIDs", ErrInvalidOptions)
+	}
+
+	// Reject requests with too many wanted collections
+	if wantedCol != nil && len(wantedCol.Prefixes)+len(wantedCol.FullPaths) > 100 {
+		return nil, fmt.Errorf("%w: too many wanted collections", ErrInvalidOptions)
+	}
+
+	return &SubscriberOptions{
+		WantedCollections: wantedCol,
+		WantedDIDs:        didMap,
+		Compress:          compress,
+		Cursor:            cursor,
+	}, nil
+}
+
+func (s *Server) AddSubscriber(ws *websocket.Conn, realIP string, opts *SubscriberOptions) (*Subscriber, error) {
+	s.lk.Lock()
+	defer s.lk.Unlock()
+
+	sub := Subscriber{
+		ws:                ws,
+		realIP:            realIP,
+		outbox:            make(chan *[]byte, 10_000),
+		hello:             make(chan struct{}),
+		id:                s.nextSub,
+		wantedCollections: opts.WantedCollections,
+		wantedDids:        opts.WantedDIDs,
+		cursor:            opts.Cursor,
+		compress:          opts.Compress,
+		deliveredCounter:  eventsDelivered.WithLabelValues(realIP),
+		bytesCounter:      bytesDelivered.WithLabelValues(realIP),
+		rl:                rate.NewLimiter(rate.Limit(s.maxSubRate), int(s.maxSubRate)),
+	}
+
+	s.Subscribers[s.nextSub] = &sub
+	s.nextSub++
+
+	subscribersConnected.WithLabelValues(realIP).Inc()
+
+	slog.Info("adding subscriber",
+		"real_ip", realIP,
+		"id", sub.id,
+		"wantedCollections", opts.WantedCollections,
+		"wantedDids", opts.WantedDIDs,
+		"cursor", opts.Cursor,
+		"compress", opts.Compress,
+	)
+
+	return &sub, nil
+}
+
+func (s *Server) RemoveSubscriber(num int64) {
+	s.lk.Lock()
+	defer s.lk.Unlock()
+
+	slog.Info("removing subscriber", "id", num, "real_ip", s.Subscribers[num].realIP)
+
+	subscribersConnected.WithLabelValues(s.Subscribers[num].realIP).Dec()
+
+	delete(s.Subscribers, num)
+}
+
+// WantsCollection returns true if the subscriber wants the given collection
+func (sub *Subscriber) WantsCollection(collection string) bool {
+	if sub.wantedCollections == nil {
+		return true
+	}
+
+	// Start with the full paths for fast lookup
+	if len(sub.wantedCollections.FullPaths) > 0 {
+		if _, match := sub.wantedCollections.FullPaths[collection]; match {
+			return true
+		}
+	}
+
+	// Check the prefixes (shortest first)
+	for _, prefix := range sub.wantedCollections.Prefixes {
+		if strings.HasPrefix(collection, prefix) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (s *Subscriber) UpdateOptions(opts *SubscriberOptions) {
+	s.lk.Lock()
+	defer s.lk.Unlock()
+
+	s.wantedCollections = opts.WantedCollections
+	s.wantedDids = opts.WantedDIDs
+	s.cursor = opts.Cursor
+	s.compress = opts.Compress
+}
+
+// Terminate sends a close message to the subscriber
+func (s *Subscriber) Terminate(reason string) error {
+	return s.ws.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, reason))
+}
+
+func (s *Subscriber) SetCursor(cursor *int64) {
+	s.lk.Lock()
+	defer s.lk.Unlock()
+
+	s.cursor = cursor
+}
